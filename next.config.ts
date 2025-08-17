2ba153b10285f07388d8cd6496bc13c7d97bbe54
Hojat Gazestani <39824268+hojat-gazestani@users.noreply.github.com>
2025-08-17 18:02:18 +0330

    init

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '',
+  assetPrefix: '',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+
+};
+
+export default nextConfig;
